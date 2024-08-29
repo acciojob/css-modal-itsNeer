ea26b5786ecb731c2b12665175073db4c2b7c861
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-08-29 12:11:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const modal= document.getElementById('modal');
+const openModalBtn= document.getElementById('openModal');
+const closeModalBtn= document.querySelector('.close-Modal');
+openModalBtn.onclick = function() {
+    modal.style.display = 'block';
+}
+closeModalBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
